7922939003840d6fb48fa5f99b042cf991e72d07
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2021-08-29 23:55:11 -0500

    Pipes personalizados: mayusculas y vuela

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Heroe, Color } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
+  enMayusculas: boolean = false;
 
-  ngOnInit(): void {
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Wonder Woman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Flash',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Cyborg',
+      vuela: true,
+      color: Color.negro
+    },
+    {
+      nombre: 'Green Arrow',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.rojo
+    }
+  ];
+
+  colorOptions = {
+    '0': 'Rojo',
+    '1': 'Negro',
+    '2': 'Azul',
+    '3': 'Verde'
+  }
+
+
+
+
+  toggleMayusculas(){
+    this.enMayusculas = !this.enMayusculas;
   }
 
 }
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -3,19 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
+// Pipes
+import { MayusculasPipe } from "./pipes/mayusculas.pipe";
+
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 
 @NgModule({
   declarations: [
+    MayusculasPipe,
+    VuelaPipe,
     NumerosComponent,
     NoComunesComponent,
     BasicosComponent,
-    OrdenarComponent
+    OrdenarComponent,
+    OrdenarPipe
   ],
   exports:[
     NumerosComponent,
